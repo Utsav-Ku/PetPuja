78d59d1508a4695d479776799ca1a8798fb3ab6c
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-04-03 12:00:09 +0530

    Update restaurant card component to display dynamic data

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div className='header'>
             <div className='logo'>
                 {/* <img src='https://static.vecteezy.com/system/resources/previews/021/953/308/original/food-ordering-app-logo-with-points-and-fork-shapes-in-the-center-free-vector.jpg' /> */}
-                <h1>Swiggy</h1>
+                <h1>PetPuja</h1>
             </div>
             <div className='nav-items'>
                 <ul>
@@ -20,34 +20,51 @@ const Header = () => {
     )
 }
 
-const ResturantCard = () => {
+const ResturantCard = ({restaurant}) => {
     return (
         <div className='res-card' style={{ backgroundColor: "#f0f0f0" }}>
             <img 
                 className='res-logo' 
                 alt='res-logo' 
-                src='https://th.bing.com/th/id/OIP.2rubt7oRrm9ukL2Px8ku6gHaEK?rs=1&pid=ImgDetMain'
+                src={restaurant.image} 
             />
-            <h3>Meghana Foods</h3>
-            <h4>Biryani, North India, Asian</h4>
-            <h4>4.4 stars</h4>
-            <h4>38 minutes</h4>
+            <h3>{restaurant.name}</h3>
+            <h4>{restaurant.cuisine}</h4>
+            <h4>{restaurant.rating} stars</h4>
+            <h4>{restaurant.deliveryTime}</h4>
         </div>
     )
 }
+
+const resList = [
+    {
+        name: "KFC",
+        cuisine: "Fast Food",
+        rating: 4.2,
+        deliveryTime: "30 minutes",
+        image: "https://th.bing.com/th/id/OIP.2rubt7oRrm9ukL2Px8ku6gHaEK?rs=1&pid=ImgDetMain",
+        id: "420"
+    },
+    {
+        name: "Meghana Foods",
+        cuisine: "Biryani",
+        rating: 4.5,
+        deliveryTime: "40 minutes",
+        image: "https://th.bing.com/th/id/OIP.2rubt7oRrm9ukL2Px8ku6gHaEK?rs=1&pid=ImgDetMain",
+        id: "231"
+    }
+]
 
 const Body = () => {
     return (
         <div className='body'>
             <div className='search'>Search</div>
             <div className='res-container'>
-                <ResturantCard />
-                <ResturantCard />
-                <ResturantCard />
-                <ResturantCard />
-                <ResturantCard />
-                <ResturantCard />
-                <ResturantCard />
+                {
+                    resList.map((restaurant) => (
+                        <ResturantCard key={restaurant.id} restaurant={restaurant} />
+                    ))
+                }
             </div>
         </div>
     )
