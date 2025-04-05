00cc6474d095c5e20e82ed3f1f16d5353119ca86
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-04-05 12:01:27 +0530

    Add filter button to display top-rated restaurants and update CSS styles

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,69 @@
 import ResturantCard from "./ResturantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+
+    //Local State Variable - super powerful variable
+    // const arr = useState([]);
+    // const [ listOfResturants, setListOfResturants ] = arr; Destructuring the array
+    // const listOfResturants = arr[0];
+    // const setListOfResturants = arr[1]; Destructuring the array
+
+    const [ listOfResturants, setListOfResturants ] = useState([
+        {
+            name: "John",
+            cuisine: "Fast Food",
+            rating: 3.8,
+            deliveryTime: "30 minutes",
+            image: "https://th.bing.com/th/id/OIP.2rubt7oRrm9ukL2Px8ku6gHaEK?rs=1&pid=ImgDetMain",
+            id: "420"
+        },
+        {
+            name: "Meghana Foods",
+            cuisine: "Biryani",
+            rating: 4.5,
+            deliveryTime: "40 minutes",
+            image: "https://th.bing.com/th/id/OIP.2rubt7oRrm9ukL2Px8ku6gHaEK?rs=1&pid=ImgDetMain",
+            id: "231"
+        }
+    ]);
+
+    //Normal JS Variable
+    let listOfResturantsJS = [
+        {
+            name: "John",
+            cuisine: "Fast Food",
+            rating: 3.8,
+            deliveryTime: "30 minutes",
+            image: "https://th.bing.com/th/id/OIP.2rubt7oRrm9ukL2Px8ku6gHaEK?rs=1&pid=ImgDetMain",
+            id: "420"
+        },
+        {
+            name: "Meghana Foods",
+            cuisine: "Biryani",
+            rating: 4.5,
+            deliveryTime: "40 minutes",
+            image: "https://th.bing.com/th/id/OIP.2rubt7oRrm9ukL2Px8ku6gHaEK?rs=1&pid=ImgDetMain",
+            id: "231"
+        }
+    ];
+
     return (
         <div className='body'>
-            <div className='search'>Search</div>
+            <div className='filter'>
+                <button className="filter-btn" 
+                    onClick={() => {
+                        //filter logic here
+                        const filteredList = listOfResturants.filter((res) => res.rating > 4);
+                        setListOfResturants(filteredList);
+                    }}>
+                    Top Rated Resturants
+                </button>
+            </div>
             <div className='res-container'>
                 {
-                    resList.map((restaurant) => (
+                    listOfResturants.map((restaurant) => (
                         <ResturantCard key={restaurant.id} restaurant={restaurant} />
                     ))
                 }
