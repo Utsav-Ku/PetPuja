489d875af9b9bca19aae48f391ac6ccbab5f199d
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-04-10 17:57:52 +0530

    Refactor constants for improved URL management

--- src/components/ResturantCard.js
+++ src/components/ResturantCard.js
@@ -1,3 +1,5 @@
+import { CDN_URL } from "../utils/constants";
+
 const ResturantCard = ({ resData }) => {
     const {
       cloudinaryImageId,
@@ -11,13 +13,8 @@ const ResturantCard = ({ resData }) => {
     return (
       <div className="res-card">
         <img
-          className="res-logo"
-          alt="restaurant-logo"
-          src={
-            cloudinaryImageId
-              ? `https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${cloudinaryImageId}`
-              : "https://via.placeholder.com/200x150?text=No+Image"
-          }
+          src={CDN_URL + cloudinaryImageId}
+          className="restaurant-img"
         />
         <h3>{name}</h3>
         <h4>{cuisines?.join(", ")}</h4>
