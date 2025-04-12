304e82eade385f9b7deb9c327b86c920bf1cac26
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-04-12 16:12:35 +0530

    creation of state variable in UserClass component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,14 +4,21 @@ class UserClass extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            count: 5,
+            count2: 2,
+        }
     }
 
     render() {
+        const { count, count2 } = this.state;
         return (
             <div className="user-card">
                 <h2>Name: {this.props.name}</h2>
                 <h3>Location: Agartala</h3>
                 <h4>Contact: test123@gmail.com</h4>
+                <h4>Count = {count}</h4>
+                <h4>Count2 = {count2}</h4>
             </div>
         )
     }
